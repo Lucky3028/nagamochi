7d9c3a5faa288f85a6ac28bfb6dcd2dac42f5a50
Lucky3028 <lucky3028@users.noreply.github.com>
2021-11-20 23:38:48 +0900

    feat: とりあえずの雛形を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use seahorse::{App, Context};
+use notify_rust::Notification;
+
+use std::{env, time, path::PathBuf};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let app = App::new(env!("CARGO_PKG_NAME"))
+        .description(env!("CARGO_PKG_DESCRIPTION"))
+        .author(env!("CARGO_PKG_AUTHORS"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .usage("nagamochi [args]")
+        .action(default_action);
+
+    app.run(args);
+}
+
+fn default_action(_: &Context) {
+    loop {
+        let p = PathBuf::from("/sys/class/power_supply/").join("BAT1/capacity");
+        let ss = nagamochi::read_capacity(p).unwrap();
+        Notification::new().summary(&format!("{}", ss)).show();
+        std::thread::sleep(time::Duration::from_secs(3));
+    }
 }
