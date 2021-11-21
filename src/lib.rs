0d4fa894cc5a2fed9c3d8f498ad78dc599243b43
Lucky3028 <lucky3028@users.noreply.github.com>
2021-11-21 23:35:37 +0900

    perf(lib.rs): writeln -> write

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ mod test {
     #[test_case("" => false)]
     fn read_capa(content: &str) -> bool {
         let mut temp_file = NamedTempFile::new().unwrap();
-        if let Err(e) = writeln!(temp_file, "{}", content) {
+        if let Err(e) = write!(temp_file, "{}", content) {
             eprintln!("Error: Failed to create temp file while testing: {:?}", e);
         };
         let path = temp_file.into_parts().1;
