1623e7c9332310723fa40509395ac2a6b5ee1c96
Lucky3028 <lucky3028@users.noreply.github.com>
2021-11-22 22:27:07 +0900

    feat(main.rs): Configを読み取ってそれぞれのTriggerに基づいて通知を行う処理を追加

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use nagamochi::Config;
 use notify_rust::Notification;
 use seahorse::{App, Context};
 use std::{env, path::PathBuf, time};
@@ -16,11 +17,39 @@ fn main() {
 
 fn default_action(_: &Context) {
     loop {
+        let config = match find_config() {
+            Ok(config) => config,
+            Err(e) => {
+                eprintln!("{:?}", e);
+                Config::default()
+            }
+        };
         let path = PathBuf::from("/sys/class/power_supply/").join("BAT1/capacity");
         let capa = nagamochi::read_capacity(path).unwrap();
-        if let Err(e) = Notification::new().summary(&format!("{}", capa)).show() {
-            eprintln!("Error: Failed to send a notification: {}", e);
-        }
-        std::thread::sleep(time::Duration::from_secs(3));
+        config.triggers.iter().filter(|trigger| trigger.is_fired(capa)).for_each(|trigger| {
+            if let Err(e) = Notification::new().summary("nagamochi").body(&trigger.message).show() {
+                eprintln!("Error: Failed to send a notification: {}", e);
+            }
+        });
+        std::thread::sleep(time::Duration::from_secs(config.check_interval));
     }
+}
+
+fn find_config() -> anyhow::Result<Config> {
+    env::var_os("HOME")
+        .map(find_config_path)
+        .map_or(anyhow::anyhow!("config not found"), |path| {
+            Config::from_file(&path.unwrap())
+        })
+}
+
+fn find_config_path(home_dir: std::ffi::OsString) -> Option<PathBuf> {
+    vec![
+        format!("{:?}/.config/nagamochi/nagamochi.yml", home_dir),
+        format!("{:?}/nagamochi.yml", home_dir),
+        "./nagamochi.yml".to_string(),
+    ]
+    .iter()
+    .map(PathBuf::from)
+    .find(|path| path.exists())
 }
