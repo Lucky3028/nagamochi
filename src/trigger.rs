591c24766ea782253119ff83b1912f562546c0e4
Lucky3028 <lucky3028@users.noreply.github.com>
2021-11-22 23:12:05 +0900

    test(Trigger): Trigger#is_firedのテストを追加

--- src/trigger.rs
+++ src/trigger.rs
@@ -20,7 +20,31 @@ impl Trigger {
         match self.when {
             TriggerType::Above => self.percentage <= capacity,
             TriggerType::Below => self.percentage >= capacity,
-            TriggerType::Equal => self.percentage == capacity
+            TriggerType::Equal => self.percentage == capacity,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use test_case::test_case;
+
+    #[test_case(TriggerType::Below, 20, 1 => true)]
+    #[test_case(TriggerType::Below, 1, 20 => false)]
+    #[test_case(TriggerType::Below, 20, 20 => true)]
+    #[test_case(TriggerType::Above, 1, 20 => true)]
+    #[test_case(TriggerType::Above, 20, 1 => false)]
+    #[test_case(TriggerType::Above, 20, 20 => true)]
+    #[test_case(TriggerType::Equal, 20, 20 => true)]
+    #[test_case(TriggerType::Equal, 20, 30 => false)]
+    fn trigger_is_fired(when: TriggerType, percentage: u8, capa: u8) -> bool {
+        let trigger = Trigger {
+            percentage,
+            when,
+            message: "".to_string(),
+        };
+
+        trigger.is_fired(capa)
     }
 }
